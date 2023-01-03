99cf206a5192d1ec1f39d988f4481063f0729f64
Mazhar iqbal siddiquee <mazhariqbal963169@gmail.com>
2023-01-03 20:55:11 +0530

    mazhar

---
+++ pallindrome.js
@@ -0,0 +1,13 @@
+let str ="mazhar";
+for(let i=1; i<=str.length;i++)
+{
+  b=b+str[i];
+}
+ if(str==b)
+ {
+ console.log("pallindrome no");
+}
+  else 
+{
+  console.log(" not a pallindrome no");
+ }
